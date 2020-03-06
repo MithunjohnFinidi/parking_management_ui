33ed0281989e17541e72d456de76fa35adbfe637
Amandeep Kaur <Aamandeepkaur2928@conestogac.on.ca>
2020-03-06 10:06:55 -0500

    added routes and 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { DashboardComponent } from './components/app-holder/dashboard/dashboard.component';
+import { LocationsComponent } from './components/app-holder/locations/locations.component';
+import { VehiclesComponent } from './components/app-holder/vehicles/vehicles.component';
+import { BookingsComponent } from './components/app-holder/bookings/bookings.component';
+import { PageNotFoundComponentComponent } from './components/app-holder/page-not-found-component/page-not-found-component.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +18,32 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     HeaderComponent,
     FooterComponent,
     NavBarComponent,
+    DashboardComponent,
+    LocationsComponent,
+    VehiclesComponent,
+    BookingsComponent,
+    PageNotFoundComponentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        { path: 'dashboard', component: DashboardComponent },
+        { path: 'locations',      component: LocationsComponent },
+        { path: 'vehicles',      component: VehiclesComponent },
+        { path: 'bookings',      component: BookingsComponent },
+        // {
+        //   path: 'heroes',
+        //   component: HeroListComponent,
+        //   data: { title: 'Heroes List' }
+        // },
+        // { path: '',
+        //   redirectTo: '/dashboard',
+        //   pathMatch: 'full'
+        // },
+        { path: '**', component: PageNotFoundComponentComponent }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
